d61d83a894d4de50ffcaac42753d3aa3a9ec03d0
matthiasdotsh <git@matthias.sh>
2025-03-23 12:32:28 +0100

    surface: linux 6.12.19 -> 6.12.20

--- microsoft/surface/common/kernel/linux-surface/default.nix
+++ microsoft/surface/common/kernel/linux-surface/default.nix
@@ -7,14 +7,14 @@ let
 
   cfg = config.microsoft-surface;
 
-  version = "6.12.19";
+  version = "6.12.20";
   kernelPatches = surfacePatches {
     inherit version;
     patchFn = ./patches.nix;
   };
   kernelPackages = linuxPackage {
     inherit version kernelPatches;
-    sha256 = "sha256-1zvwV77ARDSxadG2FkGTb30Ml865I6KB8y413U3MZTE=";
+    sha256 = "sha256-Iw6JsHsKuC508H7MG+4xBdyoHQ70qX+QCSnEBySbasc=";
     ignoreConfigErrors=true;
   };
 
